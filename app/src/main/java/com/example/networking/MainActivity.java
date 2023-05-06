082215dd3bc064b7867e5197cf500381721b9e21
Chris <b22chrmo@student.his.se>
2023-05-06 21:28:08 +0200

    Added adapter.notifyDataSetChanged() to notify the adapter.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -5,9 +5,15 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
@@ -23,16 +29,29 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
 
         new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<MountainItem>>() {}.getType();
+                mtList = gson.fromJson(json, type);
+
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mtList, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(MountainItem item) {
+                Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView view = findViewById(R.id.recycler_view);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
+        view.setAdapter(adapter);
     }
-    RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mtList, new RecyclerViewAdapter.OnClickListener() {
-        @Override
-        public void onClick(MountainItem item) {
-            Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_SHORT).show();
-        }
-    });
+
 }
