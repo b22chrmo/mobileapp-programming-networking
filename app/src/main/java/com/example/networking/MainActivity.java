acc9590d19e4b9f290aa27cc4310b6cfcd42e86d
Chris <b22chrmo@student.his.se>
2023-05-06 20:47:07 +0200

    Added recyclerview, added an arraylist, added a recyclerviewadapter and viewholder.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+
+    ArrayList<MountainItem> mtList;
+
+    RecyclerViewAdapter rvAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +31,10 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
     }
-
+    RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mtList, new RecyclerViewAdapter.OnClickListener() {
+        @Override
+        public void onClick(MountainItem item) {
+            Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_SHORT).show();
+        }
+    });
 }
---
+++ app/src/main/java/com/example/networking/MountainItem.java
@@ -0,0 +1,15 @@
+package com.example.networking;
+
+@SuppressWarnings("WeakerAccess")
+public class MountainItem {
+
+    private String name;
+
+    public MountainItem(String title) {
+        this.name = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
